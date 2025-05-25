e96445555413550a8cb12840e234e038d607d2c5
Kim Kudsk <kim.kudsk@gmail.com>
2025-05-25 23:09:11 +0200

    Sidebar 1.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Link, Navlink } from "react-router-dom";
+import { SiShopware } from "react-icons/si";
+import { MdOutlineCancel } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+import { links } from "../data/dummy.js";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const activeMenu = true;
+
+  return (
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pd-10">
+      {activeMenu && (
+        <>
+          <div className="flex justify-between items-center">
+            <Link to="/" onClick={() => {}} className="items-center">
+              <SiShopware /> <span>Shoppy</span>
+            </Link>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Sidebar;
